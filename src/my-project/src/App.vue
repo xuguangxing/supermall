501cffe635c0681ca098fa4c5e91784369188db0
xuguangxing <1832377033@qq.com>
2021-03-15 16:35:37 +0800

    vue commit

---
+++ src/my-project/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <MainTabBar></MainTabBar>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+
+  import MainTabBar from "./components/mainTabBar/MainTabBar";
+
+  export default {
+    name: 'App',
+    components: {
+      MainTabBar
+    },
+  }
+</script>
+
+<style>
+
+  @import "./assets/css/base.css";
+
+
+</style>
---
+++ src/my-project/src/components/mainTabBar/MainTabBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <tab-bar>
+    <tab-bar-item path="/home" activeColor="deepPink">
+      <img slot="item-icon" src="~assets/img/tabbar/home.svg">
+      <img slot="item-icon-active" src="~assets/img/tabbar/home_active.svg">
+      <div slot="item-text">首页</div>
+    </tab-bar-item>
+    <tab-bar-item path="/category" activeColor="deepPink">
+      <img slot="item-icon" src="~assets/img/tabbar/category.svg">
+      <img slot="item-icon-active" src="~assets/img/tabbar/category_active.svg">
+      <div slot="item-text">分类</div>
+    </tab-bar-item>
+    <tab-bar-item path="/cart" activeColor="deepPink">
+      <img slot="item-icon" src="~assets/img/tabbar/shopcart.svg">
+      <img slot="item-icon-active" src="~assets/img/tabbar/shopcart_active.svg">
+      <div slot="item-text">购物车</div>
+    </tab-bar-item>
+    <tab-bar-item path="/profile" activeColor="deepPink">
+      <img slot="item-icon" src="~assets/img/tabbar/profile.svg">
+      <img slot="item-icon-active" src="~assets/img/tabbar/profile_active.svg">
+      <div slot="item-text">我的</div>
+    </tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+
+  import TabBarItem from "../tabbar/TabBarItem";
+  import TabBar from "../tabbar/TabBar";
+
+  export default {
+    name: "MainTabBar",
+    components: {
+      TabBarItem,
+      TabBar
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
