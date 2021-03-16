d0377532b94399964aa74088aaaa3bbd20dadf71
xuguangxing <1832377033@qq.com>
2021-03-16 15:57:22 +0800

    vue 提交

--- src/supermall/src/views/home/Home.vue
+++ src/supermall/src/views/home/Home.vue
@@ -3,9 +3,18 @@
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
     </nav-bar>
-
     <home-swiper :banners="banners"></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
+    <feature-view></feature-view>
+    <tab-control :titles="['流行','新款','精选']" class="tab-control"></tab-control>
+    <ul>
+      <li>f</li>
+      <li>f</li>
+      <li>f</li>
+      <li>f</li>
+      <li>f</li>
+      <li>f</li>
+    </ul>
   </div>
 
 </template>
@@ -13,8 +22,12 @@
 <script>
 
   import NavBar from "../../components/common/navbar/NavBar";
+
   import HomeSwiper from "./childComps/HomeSwiper";
   import RecommendView from "./childComps/RecommendView";
+  import FeatureView from "./childComps/FeatureView";
+  import TabControl from "../../components/content/tabControl/TabControl";
+
   import {getHomeMultidata} from "../../network/home";
 
 
@@ -23,12 +36,19 @@
     components: {
       NavBar,
       HomeSwiper,
-      RecommendView
+      RecommendView,
+      FeatureView,
+      TabControl
     },
     data() {
       return {
         banners: [],
         recommends: [],
+        good:{
+          'pop': {page:0,list: []},
+          'news': {page:0,list: []},
+          'sell': {page:0,list: []}
+        }
       }
     },
     created() {
@@ -44,8 +64,22 @@
 
 <style scoped>
 
+  #home {
+    padding-top: 44px;
+  }
+
   .home-nav {
     background-color: var(--color-tint);
     color: #fff;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+  .tab-control{
+    position: sticky;
+    top: 44px;
   }
 </style>
