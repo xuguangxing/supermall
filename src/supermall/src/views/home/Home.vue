37da74ebee774cbc9dbd788483d6209c363c0a7a
xuguangxing <1832377033@qq.com>
2021-03-16 14:24:34 +0800

    vue 提交

--- src/supermall/src/views/home/Home.vue
+++ src/supermall/src/views/home/Home.vue
@@ -5,6 +5,7 @@
     </nav-bar>
 
     <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
   </div>
 
 </template>
@@ -13,15 +14,16 @@
 
   import NavBar from "../../components/common/navbar/NavBar";
   import HomeSwiper from "./childComps/HomeSwiper";
+  import RecommendView from "./childComps/RecommendView";
   import {getHomeMultidata} from "../../network/home";
-
 
 
   export default {
     name: "Home",
     components: {
       NavBar,
-      HomeSwiper
+      HomeSwiper,
+      RecommendView
     },
     data() {
       return {
