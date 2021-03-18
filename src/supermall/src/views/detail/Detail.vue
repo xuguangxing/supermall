c28af0ab11ffe5494c631b44e05ef7ccaa85d66d
xuguangxing <1832377033@qq.com>
2021-03-18 11:37:51 +0800

    vue 提交

--- src/supermall/src/views/detail/Detail.vue
+++ src/supermall/src/views/detail/Detail.vue
@@ -7,6 +7,7 @@
       <detail-shop-info :shop="shop"></detail-shop-info>
       <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad"/>
       <detail-param-info :param-info="paramInfo"/>
+      <detail-comment-info :comment-info="commentInfo"></detail-comment-info>
     </scroll>
   </div>
 </template>
@@ -20,10 +21,10 @@
   import DetailShopInfo from "./childComps/DetailShopInfo";
   import DetailGoodsInfo from "./childComps/DetailGoodsInfo";
   import DetailParamInfo from "./childComps/DetailParamInfo";
-
+  import DetailCommentInfo from "./childComps/DetailCommentInfo";
 
   import Scroll from "../../components/common/scroll/Scroll";
-  import {getDetail, Goods, Shop,GoodsParam} from "../../network/detail";
+  import {getDetail, Goods, GoodsParam, Shop} from "../../network/detail";
 
   export default {
     name: "Detail",
@@ -34,7 +35,8 @@
         goods: {},
         shop: {},
         detailInfo: {},
-        paramInfo: {}
+        paramInfo: {},
+        commentInfo: {}
 
       }
     },
@@ -45,12 +47,14 @@
       DetailShopInfo,
       DetailGoodsInfo,
       DetailParamInfo,
+      DetailCommentInfo,
       Scroll
     },
     created() {
       this.iid = this.$route.params.iid
       getDetail(this.iid).then(res => {
         const data = res.result;
+        console.log(data)
         this.topImages = data.itemInfo.topImages
 
         // 2.获取商品信息
@@ -64,13 +68,20 @@
 
         // 5.获取参数的信息
         this.paramInfo = new GoodsParam(data.itemParams.info, data.itemParams.rule)
+
+        if (data.rate.cRate != 0) {
+          console.log(data.rate.list[0])
+          this.commentInfo = data.rate.list[0];
+        }
+
+
       })
     },
-  methods: {
-    imageLoad() {
-      this.$refs.scroll.refresh()
+    methods: {
+      imageLoad() {
+        this.$refs.scroll.refresh()
+      }
     }
-  }
   }
 </script>
 
@@ -81,11 +92,13 @@
     background-color: #fff;
     height: 100vh;
   }
+
   .detail-nav {
     position: relative;
     z-index: 9;
     background-color: #fff;
   }
+
   .content {
     height: calc(100% - 44px);
   }
---
+++ src/supermall/src/views/detail/childComps/DetailCommentInfo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>评论</div>
+</template>
+
+<script>
+  export default {
+    name: "DetailCommentInfo",
+    props: {
+      commentInfo: {
+        type: Object,
+        default() {
+          return {}
+        }
+      }
+
+    }
+
+  }
+</script>
+
+<style scoped>
+
+</style>
